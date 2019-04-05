559c661251930792f398d7615f1b1f2b91f336a3
wheelman1995 <anikinvitya@gmail.com>
2019-04-05 11:07:53 +0300

    add travis

--- app/src/main/java/ru/wheelman/github/view/MainActivity.kt
+++ app/src/main/java/ru/wheelman/github/view/MainActivity.kt
@@ -3,6 +3,8 @@ package ru.wheelman.github.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 import ru.wheelman.github.R
 import ru.wheelman.github.databinding.ActivityMainBinding
 
@@ -10,6 +12,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(
+            Fabric.Builder(this)
+                .debuggable(true)
+                .kits(Crashlytics())
+                .build()
+        )
         val binding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         setSupportActionBar(binding.toolbar)
